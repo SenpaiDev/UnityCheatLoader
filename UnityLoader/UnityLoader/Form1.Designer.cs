18f7c6f1492abab6209e1ef13be7ddd6a6e2761a
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-23 18:13:43 +0100

    Commit

--- UnityLoader/UnityLoader/Form1.Designer.cs
+++ UnityLoader/UnityLoader/Form1.Designer.cs
@@ -23,21 +23,35 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 236);
+            this.listBox1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 236);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- UnityLoader/UnityLoader/Form1.cs
+++ UnityLoader/UnityLoader/Form1.cs
@@ -7,15 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net;
 namespace UnityLoader {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e) {
+            using (WebClient client = new WebClient()) {
+                string supported_games = client.DownloadString("http://senpaidev.github.io/UnityCheatLoader/").Split(new string[] { "<ul>" }, StringSplitOptions.None)[1].Split(new string[] { "</ul>" }, StringSplitOptions.None)[0];
+                int NumOfSupGames = supported_games.Split(new string[] {"<li>"}, StringSplitOptions.None).Length - 1;
+                for (int x = 1; x <= NumOfSupGames; x++) {
+                    listBox1.Items.Add(supported_games.Split(new string[]{ "<li>" }, StringSplitOptions.None)[x].Split(new string[] {"</li>"}, StringSplitOptions.None)[0]);
+                }
+            }
+        }
 
+        private void Form1_DoubleClick(object sender, EventArgs e) {
+            if(listBox1.SelectedItem != null) {
+
+            }
         }
     }
 }
